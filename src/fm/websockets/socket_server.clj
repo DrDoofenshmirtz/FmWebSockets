2529045d6a56aef78b608e705d55ebf7b0ad59aa
Frank <frank@perry.(none)>
2012-04-22 14:56:20 +0200

    Safely close unused sockets, skip "accept" errors.

--- src/fm/websockets/socket_server.clj
+++ src/fm/websockets/socket_server.clj
@@ -4,34 +4,38 @@
   fm.websockets.socket-server
   (:use
     [clojure.contrib.def :only (defvar-)]
+    [fm.core.lazy-seqs :only (take-until)]
     [fm.core.exception :only (do-silently close-silently error?)])
   (:import
     (java.net ServerSocket)))
 
 (defvar- closed-tag (Object.))
 
+(defn- close-if-socket [socket]
+  (if-not (or (= closed-tag socket) (error? socket))
+    (close-silently socket)))
+
 (defn- create-socket! [socket-access port]
-  (socket-access
-    (fn [current-socket]
-      (if-not (= closed-tag @current-socket)
-        (let [socket (do-silently (ServerSocket. port))]
-          (reset! current-socket socket))))))
+  (let [[old-socket new-socket]
+        (socket-access
+          (fn [current-socket]
+            (let [old-socket @current-socket]
+              [old-socket
+               (if-not (= closed-tag old-socket)
+                 (let [new-socket (do-silently (ServerSocket. port))]
+                   (reset! current-socket new-socket)))])))]
+    (close-if-socket old-socket)
+    new-socket))
 
 (defn- server-socket-seq [socket-access port]
   (take-while identity (repeatedly #(create-socket! socket-access port))))
 
-;; TODO: skip failures!
-(defn- wait-for-client-socket [server-socket]
-  (if-let [client-socket (do-silently (.accept server-socket))]
-    (if-not (.isClosed server-socket)
-      client-socket)))
-
 (defn- client-socket-seq [server-socket]
   (if (error? server-socket)
     [server-socket]
-    (take-while
-      identity
-      (repeatedly #(wait-for-client-socket server-socket)))))
+    (take-until
+      error?
+      (repeatedly #(do-silently (.accept server-socket))))))
 
 (defn- connection-seq [server-sockets]
   (when-first [server-socket server-sockets]
@@ -43,9 +47,9 @@
   (if-let [socket (socket-access
                     (fn [current-socket]
                       (let [socket @current-socket]
-                        (reset! current-socket closed-tag))))]
-    (if-not (or (= closed-tag socket) (error? socket))
-      (close-silently socket))))
+                        (reset! current-socket closed-tag)
+                        socket)))]
+    (close-if-socket socket)))
 
 (defn- protected-access [mutable]
   (let [lock (Object.)]
@@ -55,15 +59,14 @@
 
 (defn- connection-producer [port]
   (let [socket-access (protected-access (atom nil))
-        server-sockets (server-socket-seq socket-access port)
-        connections (connection-seq server-sockets)
-        close #(close-socket! socket-access)]
-    {:connections connections :close! close}))
+        server-sockets (server-socket-seq socket-access port)]
+    {:connections (connection-seq server-sockets)
+     :close! #(close-socket! socket-access)}))
 
 (def cp (connection-producer 8080))
 
-(future
-  (Thread/sleep 200)
+@(future
+  (Thread/sleep 100)
   (println "closing...")
   (println ((:close! cp)))
   (println "closed!"))
