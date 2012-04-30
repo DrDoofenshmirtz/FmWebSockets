93efc1e7b27984564dab701a12d672eb703cc0fb
Frank <frank@perry.(none)>
2012-04-30 15:17:08 +0200

    Finalized version of the socket server.

--- src/fm/websockets/socket_server.clj
+++ src/fm/websockets/socket_server.clj
@@ -6,7 +6,7 @@
     [clojure.contrib.def :only (defvar-)]
     [fm.core.lazy-seqs :only (take-until)]
     [fm.core.exception :only (do-silently close-silently error?)]
-    [fm.core.threading :only (guarded-ref)])
+    [fm.core.threading :only (guarded-ref do-async)])
   (:import
     (java.net ServerSocket)))
 
@@ -62,13 +62,72 @@
 (defn- stop-production! [connection-producer]
   (close-socket! (:socket-access connection-producer)))
 
-(def cp (connection-producer 8080))
+(defn- close-connections! [connections]
+  (if-let [open-connections (seq (dosync
+                                   (let [open-connections @connections]
+                                     (ref-set connections nil)
+                                     open-connections)))]
+    (reduce
+      #(if (error? %1) %1 %2)
+      (map #(close-silently %) open-connections))))
 
-(future
-  (Thread/sleep 500)
-  (println "Stopping production...")
-  (println (stop-production! cp))
-  (println "...stopped!"))
+(defn- log-error [error]
+  (binding [*out* *err*]
+    (println error)))
 
-(doseq [connection (:connections cp)]
-  (println connection))
+(defn- safe-connection-handler [connection-handler]
+  (fn [connection]
+    (let [result (do-silently (connection-handler connection))
+          closed? (.isClosed connection)]
+      (close-silently connection)
+      (when (error? result)
+        (log-error "Unhandled error in fm.websockets.socket-server!")
+        (log-error result)
+        (if closed?
+          (log-error "--- The connection has been closed! ---")))
+      result)))
+
+(defn- handle-connection [connection-handler connection connections]
+  (when (dosync (alter connections #(and % (conj % connection))))
+    (do-async
+      [(connection-handler connection)
+       (dosync (alter connections #(and % (disj % connection))))]
+      :thread-name "fm.websockets.socket-server.handle-connection")
+    connection))
+
+(defn- connection-processor [connection-handler error-handler connections]
+  (let [connection-handler (safe-connection-handler connection-handler)]
+    (fn [connection]
+      (if connection
+        (if-not (error? connection)
+          (handle-connection connection-handler connection connections)
+          (error-handler connection))))))
+
+(defn- handle-connection-error [error]
+  (log-error "Connection error in fm.websockets.socket-server!")
+  (log-error error))
+
+(defn start-up
+  ([port connection-handler]
+    (start-up port connection-handler handle-connection-error))
+  ([port connection-handler error-handler]
+    (let [error-handler (or error-handler handle-connection-error)
+          connection-producer (connection-producer port)
+          connections (ref (hash-set))
+          shut-down (fn []
+                      (stop-production! connection-producer)
+                      (close-connections! connections))
+          connection-processor (connection-processor
+                                 connection-handler
+                                 error-handler
+                                 connections)]
+      (do-async
+        [(loop [connections (:connections connection-producer)]
+           (if (connection-processor (first connections))
+             (recur (rest connections))
+             (shut-down)))]
+        :thread-name "fm.websockets.socket-server.wait-for-clients")
+      shut-down)))
+
+(defn shut-down [socket-server]
+  (and socket-server (socket-server)))
