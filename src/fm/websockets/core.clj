da2027d8d014084efce09df4de8c0184283be887
Frank <frank@perry.(none)>
2012-06-05 13:51:43 +0200

    WebSocket specific server.

--- src/fm/websockets/core.clj
+++ src/fm/websockets/core.clj
@@ -1,31 +1,17 @@
 (ns fm.websockets.core
   (:use
-    [fm.core.lazy-seqs :only (unsigned-byte-seq)]
-    [fm.websockets.socket-server :only (start-up)]
-    [fm.websockets.protocol :only (read-connect-request
-                                   write-connect-response
-                                   send-object
-                                   message-seq-seq)]))
+    [fm.websockets.server :only (start-up)]
+    [fm.websockets.protocol :only (send-object)]
+    [fm.websockets.connection :only (with-output)]))
 
-(defn- dump-bytes [byte-seq]
-  (loop [byte-seq byte-seq next-byte (first byte-seq)]
-    (when next-byte
-      (println next-byte "=" (Integer/toBinaryString next-byte))
-      (recur (rest byte-seq) (first (rest byte-seq))))))
+(defn- send-greeting-and-print-messages [connection]
+  (with-output connection
+    (send-object %out {:id 1 :result "Connected!"}))
+  (loop [messages (:messages connection)]
+    (if-let [message (first messages)]
+      (do
+        (println message)
+        (recur (rest messages)))
+      (println "Connection closed. Bye!"))))
 
-(defn- read-http-message [socket]
-  (let [input-stream (.getInputStream socket)
-        output-stream (.getOutputStream socket)
-        byte-seq (unsigned-byte-seq input-stream)
-        [connect-request byte-seq] (read-connect-request byte-seq)]
-    (println connect-request)
-    (write-connect-response output-stream connect-request)
-    (send-object output-stream {:ticket 42 :result "Guten Tag, Mausi!" :ok true})
-    (loop [message-seq-seq (message-seq-seq byte-seq)]
-      (if-let [message-seq (first message-seq-seq)]
-        (do
-          (println message-seq)
-          (recur (rest message-seq-seq)))
-        (println "Connection closed. Bye!")))))
-
-(start-up 17500 read-http-message)
+(start-up 17500 send-greeting-and-print-messages)
--- src/fm/websockets/server.clj
+++ src/fm/websockets/server.clj
@@ -1,64 +1,19 @@
-(ns fm.websockets.server
+(ns
+  ^{:doc "A socket server for the HTML5 WebSockets protocol."
+    :author "Frank Mosebach"}
+  fm.websockets.server
+  (:require
+    [fm.websockets.socket-server :as socket-server])
   (:use
-    [fm.core.exception :only (error? do-silently)]
-    [fm.core.threading :only (do-async)]
-    [fm.comm.server-connector :only (server-connector close!)]))
+    [fm.websockets.connection :only (connect)]))
 
-(defn- shut-down-connections [connections]
-  (if-let [open-connections (seq (dosync
-                                   (let [open-connections @connections]
-                                     (ref-set connections nil)
-                                     open-connections)))]
-    (reduce 
-      #(if (error? %1) %1 %2)
-      (map #(.close %) open-connections))))
+(defn- wrap-connection-handler [connection-handler]
+  (fn [socket]
+    (connection-handler (connect socket))))
 
-(defn- socket-server [port]
-  {:connector (server-connector port) :connections (ref (hash-set))})
-
-(defn- connection-seq [connector]
-  (take-while identity (repeatedly connector)))
-
-(defn- handle-connection [connections connection connection-handler]
-  (when (dosync (alter connections #(and % (conj % connection))))
-    (do-async
-      [(connection-handler connection)
-       (dosync (alter connections #(and % (disj % connection))))]
-      :thread-name "fm.websockets.server.handle-connection")
-    connection))
-
-(defn- safe-connection-handler [connection-handler]
-  (fn [connection]
-    (let [result (do-silently (connection-handler connection))]
-      (.close connection)
-      result)))
-
-(defn- connection-processor [connections connection-handler error-handler]
-  (let [connection-handler (safe-connection-handler connection-handler)]
-    (fn [connection]
-      (if connection
-        (if-not (error? connection)
-          (handle-connection connections connection connection-handler)
-          (error-handler connection))))))
-
-(defn shut-down [socket-server]
-  (if socket-server
-    (let [{connector :connector connections :connections} socket-server]
-      (close! connector)
-      (shut-down-connections connections))))
-
-(defn start-up [port connection-handler error-handler]
-  (let [{connector :connector
-         connections :connections
-         :as server} (socket-server port)        
-        connection-processor (connection-processor
-                               connections
-                               connection-handler
-                               error-handler)]
-    (do-async
-      [(loop [connection-seq (connection-seq connector)]
-        (if (connection-processor (first connection-seq))
-          (recur (rest connection-seq))
-          (shut-down server)))]
-      :thread-name "fm.websockets.server.wait-for-clients")
-    server))
+(defn start-up
+  ([port connection-handler]
+    (start-up port connection-handler nil))
+  ([port connection-handler error-handler]
+    (let [connection-handler (wrap-connection-handler connection-handler)]
+      (socket-server/start-up port connection-handler error-handler))))
