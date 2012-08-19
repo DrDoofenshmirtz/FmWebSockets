e5665e5951fb35dd7b008ec24b59da9dcec9a3ac
Frank <frank@perry.(none)>
2012-08-19 16:24:50 +0200

    Generate fm.websockets.Server class.

--- src/fm/websockets/server.clj
+++ src/fm/websockets/server.clj
@@ -2,9 +2,13 @@
   ^{:doc "A socket server for the HTML5 WebSockets protocol."
     :author "Frank Mosebach"}
   fm.websockets.server
+  (:gen-class
+    :name fm.websockets.Server
+    :main true)
   (:require
     [fm.websockets.socket-server :as socket-server])
   (:use
+    [clojure.contrib.command-line :only (with-command-line)]
     [fm.websockets.connection :only (connect)]))
 
 (defn- wrap-connection-handler [connection-handler]
@@ -17,3 +21,11 @@
   ([port connection-handler error-handler]
     (let [connection-handler (wrap-connection-handler connection-handler)]
       (socket-server/start-up port connection-handler error-handler))))
+
+(defn -main [& args]
+  (with-command-line args
+    "WsServer"
+    [[port "The WebSocket server's port number" 17500]
+     [connection-handler "Fully qualified name of a connection handler function"]
+     [error-handler "Fully qualified name of an error handler function"]]
+    (println (str "Starting WsServer on port " port "..."))))
