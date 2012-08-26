26e1cf93b4fe07dbf2e7bc1089caa8a40b76f89a
Frank <frank@perry.(none)>
2012-08-26 11:51:03 +0200

    Alter the connection instead of a nested ref type.
    Demonstrates usage of the result wrapper function.

--- src/fm/websockets/samples/counter/counter_service.clj
+++ src/fm/websockets/samples/counter/counter_service.clj
@@ -1,10 +1,12 @@
 (ns
   ^{:doc "A demo namespace to be used with a JSON namespace RPC dispatcher."
     :author "Frank Mosebach"}
-  fm.websockets.samples.counter.counter-service)
+  fm.websockets.samples.counter.counter-service
+  (:use [fm.websockets.json-rpc :only (result)]))
 
 (defn reset-counter [connection value]
-  (reset! (:counter connection) value))
+  (result (assoc connection :counter value) value))
 
 (defn inc-counter [connection increment]
-  (swap! (:counter connection) + increment))
+  (let [value (+ (:counter connection) increment)]
+    (result (assoc connection :counter value) value)))
