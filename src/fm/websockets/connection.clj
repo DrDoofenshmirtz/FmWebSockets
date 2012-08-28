2b891c4039913892bb6fedef0f1a8958e7171d42
Frank <frank@perry.(none)>
2012-08-28 13:38:49 +0200

    Add the WebSockets connect request to the connection properties.

--- src/fm/websockets/connection.clj
+++ src/fm/websockets/connection.clj
@@ -15,16 +15,19 @@
 (defn- make-output [output-stream]
   (vary-meta (guarded-access output-stream) assoc :type ::output))
 
-(defn- make-connection [byte-seq output-stream]
+(defn- make-connection [connect-request byte-seq output-stream]
   (vary-meta
-    {:messages (message-seq byte-seq)
+    {:request connect-request
+     :messages (message-seq byte-seq)
      :output (make-output output-stream)}
     assoc :type ::connection))
 
 (defn connect
   "Tries to establish a WebSocket connection, assuming the given socket is
   connected to a WebSocket client.
-  Returns a map {:messages lazy-seq-of-incoming-messages
+  Returns a map {:request {:request-line connect-request-line
+                           :request-headers connect-request-headers}
+                 :messages lazy-seq-of-incoming-messages
                  :output   guarded-access-to-connection-output}."
   [socket]
   (let [input-stream (.getInputStream socket)
@@ -32,7 +35,7 @@
         byte-seq (unsigned-byte-seq input-stream)
         [connect-request byte-seq] (read-connect-request byte-seq)]
     (write-connect-response output-stream connect-request)
-    (make-connection byte-seq output-stream)))
+    (make-connection connect-request byte-seq output-stream)))
 
 (defn take-message
   "Takes the next message from the given connection's lazy message sequence.
