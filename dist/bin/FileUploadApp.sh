83d9a0b087fa7d6965113801f59f5195545d39df
Frank <frank@perry.(none)>
2012-11-06 21:58:48 +0100

    Increased heap size to 250 MB.

--- dist/bin/FileUploadApp.sh
+++ dist/bin/FileUploadApp.sh
@@ -9,7 +9,7 @@ fi
 
 SCRIPT_HOME=`dirname $SCRIPT_HOME`
 
-java -server -Xms25m -Xmx25m -cp "${SCRIPT_HOME}/../lib/*" \
+java -server -Xms250m -Xmx250m -cp "${SCRIPT_HOME}/../lib/*" \
 -Djava.util.logging.config.file="${SCRIPT_HOME}/../config/FileUploadApp.logging.properties" \
 fm.websockets.samples.fileupload.FileUploadApp $*
 
