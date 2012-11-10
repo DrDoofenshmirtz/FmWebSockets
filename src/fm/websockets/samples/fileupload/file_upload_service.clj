b9239a1e8d397a5d25cf461989dd42515b19c1ff
FrankM <frank.mosebach@gmx.de>
2012-11-10 21:57:22 +0100

    Get the user home from the system properties and create a unique subdirectory for each uploaded file.

--- src/fm/websockets/samples/fileupload/file_upload_service.clj
+++ src/fm/websockets/samples/fileupload/file_upload_service.clj
@@ -8,7 +8,7 @@
                                     remove-resources
                                     update-resources)])
   (:import
-    (java.io File FileOutputStream)
+    (java.io File FileOutputStream IOException)
     (java.util UUID)))
 
 (defn- data-bytes [data]
@@ -30,12 +30,22 @@
   (if delete?
     (.delete file)))
 
+(defn- create-upload-directory [id]
+  (let [directory (File. (System/getProperty "user.home") "Uploads")
+        directory (File. directory id)]
+    (if (or (.exists directory) (not (.mkdirs directory)))
+      (throw (IOException. "Failed to create upload directory!"))
+      directory)))
+
+(defn- make-resource [id file-name]
+  (let [file   (File. (create-upload-directory id) file-name)
+        output (FileOutputStream. file)]
+    {:file file :output output}))
+
 (defn- start-upload [connection upload]
   (let [{file-name :fileName data :data} upload
-        file     (File. "/home/frank/Uploads" file-name)
-        output   (FileOutputStream. file)
-        id       (str (UUID/randomUUID))
-        resource {:file file :output output}]
+        id (str (UUID/randomUUID))
+        {output :output :as resource} (make-resource id file-name)]
     (manage-resource connection
                      id resource
                      :connection
