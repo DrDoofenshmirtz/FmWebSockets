0cece6bcaa1cd98a59be5018f6b149bf4bbd8a95
FrankM <frank.mosebach@gmx.de>
2012-11-10 15:26:07 +0100

    Use standard (remote) repositories for dependencies + comments.

--- project.clj
+++ project.clj
@@ -1,19 +1,23 @@
-(def local-repo-path (-> (java.io.File. "/home/frank/local-mvn-repo") .toURI str))
+;; Leiningen project file for the FmWebsockets clojure project.
+;;
+;; Additional dependencies (must be located the "lib" folder):
+;; - fm-core.jar
 
 (defproject fm/websockets "1.0.0"
   :description "FmWebSockets: Clojure HTML5 WebSockets."
-  :dependencies [[fm.clojure/clojure "1.2.0"]
-                 [fm.clojure/clojure-contrib "1.2.0"]
-                 [fm.jakarta-commons/commons-codec "1.6"]
-                 [fm/fm-core "1.0.4"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [commons-codec/commons-codec "1.6"]]
   :aot [fm.websockets.samples.counter.counter-app
         fm.websockets.samples.memprof.memory-profiling-app
         fm.websockets.samples.fileupload.file-upload-app
         fm.websockets.resources.types]
+  ; don't sweep the "lib" folder when fetching the deps, because
+  ; this would delete the additional deps.  
   :disable-deps-clean true    
-  :repositories {"fm-local" ~local-repo-path}
-  :jar-name "fm-websockets.jar"
+  :jar-name "fm-websockets.jar"  
   :omit-source false
   :jar-exclusions [#"(?:^|/).svn/" 
                    #"(?:^|/).git/" 
                    #"(?:^|/)project.clj"])
+
