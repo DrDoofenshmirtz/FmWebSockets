cf26506d9895a85a56ac134d1991da88276bd225
FrankM <frank.mosebach@gmx.de>
2012-12-08 15:40:14 +0100

    Created domain specific WebSocketException to be used as a base type, EndOfData to signal end of data.

--- src/fm/websockets/exceptions.clj
+++ src/fm/websockets/exceptions.clj
@@ -4,7 +4,7 @@
   fm.websockets.exceptions)
 
 (gen-class
-  :name fm.websockets.exceptions.ConnectionFailed
+  :name fm.websockets.exceptions.WebSocketException
   :extends java.lang.RuntimeException
   :constructors {[] []
                  [String] [String]
@@ -12,10 +12,25 @@
                  [String Throwable] [String Throwable]})
 
 (gen-class
-  :name fm.websockets.exceptions.ConnectionClosed
-  :extends java.lang.RuntimeException
+  :name fm.websockets.exceptions.ConnectionFailed
+  :extends fm.websockets.exceptions.WebSocketException
   :constructors {[] []
                  [String] [String]
                  [Throwable] [Throwable]
                  [String Throwable] [String Throwable]})
 
+(gen-class
+  :name fm.websockets.exceptions.ConnectionClosed
+  :extends fm.websockets.exceptions.WebSocketException
+  :constructors {[] []
+                 [String] [String]
+                 [Throwable] [Throwable]
+                 [String Throwable] [String Throwable]})
+
+(gen-class
+  :name fm.websockets.exceptions.EndOfData
+  :extends fm.websockets.exceptions.WebSocketException
+  :constructors {[] []
+                 [String] [String]
+                 [Throwable] [Throwable]
+                 [String Throwable] [String Throwable]})
