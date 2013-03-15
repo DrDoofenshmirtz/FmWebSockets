0e0ddc036a8ee9aa75ea32e4fe536485c0540bf6
DrDoofenshmirtz <frank.mosebach@gmx.de>
2013-03-15 22:19:26 +0100

    Updated project file to work with Leiningen 2.0.

--- project.clj
+++ project.clj
@@ -1,20 +1,25 @@
 ;; Leiningen project file for the FmWebSockets clojure project.
 ;;
-;; Additional dependencies (must be located the "lib" folder):
-;; - fm-core.jar
-;; - fm-resources.jar
+;; Additional dependencies:
+;;   fm-core.jar
+;;   fm-resources.jar
+
+(def local-mvn-repo-path (-> "/home/DrDoofenshmirtz/maven-repository"
+                             java.io.File.
+                             .toURI
+                             str))
 
 (defproject fm/websockets "1.0.0"
   :description "FmWebSockets: Clojure HTML5 WebSockets."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [commons-codec/commons-codec "1.6"]]
+                 [commons-codec/commons-codec "1.6"]
+                 [fm/fm-core "1.0.0"]
+                 [fm/fm-resources "1.0.0"]]
+  :repositories {"mvn-local" ~local-mvn-repo-path}                 
   :aot [fm.websockets.exceptions
-        fm.websockets.samples.fileupload.file-upload-app]
-  ; don't sweep the "lib" folder when fetching the deps, because
-  ; this would delete the additional deps.  
-  :disable-deps-clean true    
-  :jar-name "fm-websockets.jar"  
+        fm.websockets.samples.fileupload.file-upload-app]       
+  :jar-name "fm-websockets.jar"
   :omit-source false
   :jar-exclusions [#"(?:^|/).svn/" 
                    #"(?:^|/).git/" 
