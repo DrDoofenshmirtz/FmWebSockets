790f143b97dafb0e5c21098a10afb5a9a2b6a6bc
DrDoofenshmirtz <frank.mosebach@gmx.de>
2013-04-06 19:50:05 +0200

    Incomplete implementation of json rpc format.

--- src/fm/websockets/rpc/json.clj
+++ src/fm/websockets/rpc/json.clj
@@ -1,20 +1,23 @@
 (ns
-  ^{:doc "Support for JSON as an rpc format."
+  ^{:doc "Support for JSON as an RPC format."
     :author "Frank Mosebach"}
   fm.websockets.rpc.json
   (:require
     [clojure.contrib.json :as json]
     [fm.websockets.protocol :as prot]
-    [fm.websockets.rpc.core :as rpc]))
+    [fm.websockets.rpc.core :as rpc]
+    [fm.websockets.rpc.format :as fmt]))
 
-(defn message->request [message]
-  (when (and message (prot/text-message? message))
-    (json/read-json (prot/message-content message))))
+(fmt/declare-format json)
 
-(defn object->content [object]
+(defn- object->content [object]
   (let [object (if (nil? object) true object)]
     (json/json-str object)))
 
 (defn connection-handler []
-  (rpc/connection-handler message->request object->content))
+  (rpc/connection-handler rpc-format))
+
+(defmethod fmt/message->request rpc-format [_ message]
+  (when (and message (prot/text-message? message))
+    (json/read-json (prot/message-content message))))
 
