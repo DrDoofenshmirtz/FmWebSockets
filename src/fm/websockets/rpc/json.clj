f830b9255b027583565644d04b6a9491f65175ab
DrDoofenshmirtz <frank.mosebach@gmx.de>
2013-04-08 08:47:37 +0200

    Provided json implementations of rpc format methods.

--- src/fm/websockets/rpc/json.clj
+++ src/fm/websockets/rpc/json.clj
@@ -10,14 +10,22 @@
 
 (fmt/declare-format json)
 
-(defn- object->content [object]
-  (let [object (if (nil? object) true object)]
-    (json/json-str object)))
-
 (defn connection-handler []
   (rpc/connection-handler rpc-format))
 
 (defmethod fmt/message->request rpc-format [_ message]
   (when (and message (prot/text-message? message))
     (json/read-json (prot/message-content message))))
+
+(defmethod fmt/result->content rpc-format [_ id result]
+  (json/json-str {:id id :result (if (nil? result) true result) :error nil}))
+
+(defmethod fmt/error->content rpc-format [_ id error]
+  (json/json-str {:id id :result nil :error (if (nil? error) true error)}))
+
+(defmethod fmt/request->content rpc-format [_ id name args]
+  (json/json-str {:id id :method name :params args}))
+
+(defmethod fmt/notification->content rpc-format [_ name args]
+  (json/json-str {:id nil :method name :params args}))
 
