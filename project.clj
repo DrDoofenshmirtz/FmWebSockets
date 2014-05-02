daafa765e5e36f51b5ed3028d862014379eb534b
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-05-02 15:08:28 +0200

    Adjusted project naming scheme and dependencies, removed local repo dependency.

--- project.clj
+++ project.clj
@@ -1,24 +1,13 @@
 ;; Leiningen project file for the FmWebSockets clojure project.
-;;
-;; Additional dependencies:
-;;   fm-core.jar
-;;   fm-resources.jar
 
-(def local-mvn-repo-path (-> (str (System/getProperty "user.home")
-                                  "/maven-repository")
-                             java.io.File.
-                             .toURI
-                             str))
-
-(defproject fm/websockets "1.0.0"
+(defproject fm/fm-websockets "1.0.0-SNAPSHOT"
   :description "FmWebSockets: Clojure HTML5 WebSockets."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [commons-codec/commons-codec "1.6"]
                  [com.sun.net.httpserver/http "20070405"]
-                 [fm/fm-core "1.0.0"]
-                 [fm/fm-resources "1.0.0"]]
-  :repositories {"mvn-local" ~local-mvn-repo-path}                 
+                 [fm/fm-core "1.0.0-SNAPSHOT"]
+                 [fm/fm-resources "1.0.0-SNAPSHOT"]]                 
   :aot [fm.websockets.exceptions
         fm.websockets.rpc.types
         fm.websockets.samples.counter.counter-app
